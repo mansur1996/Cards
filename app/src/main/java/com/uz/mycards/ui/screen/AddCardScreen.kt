19162c720ce85828b42698ddc4a48340805b0d9e
mirzayev96 <mirzayevmn96@gmail.com>
2023-02-08 15:20:14 +0500

    15:20 08/02/2023

--- app/src/main/java/com/uz/mycards/ui/screen/AddCardScreen.kt
+++ app/src/main/java/com/uz/mycards/ui/screen/AddCardScreen.kt
@@ -2,6 +2,8 @@ package com.uz.mycards.ui.screen
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
@@ -23,6 +25,9 @@ class AddCardScreen : Fragment(R.layout.screen_add_card) {
 
     private val binding by viewBinding(ScreenAddCardBinding::bind)
     private val viewModel: AddCardViewModel by viewModels<AddCardViewModelImpl>()
+    private var isValidValiditDate = false
+    private var isCardNumber = false
+    private var isCardDate = false
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -32,18 +37,18 @@ class AddCardScreen : Fragment(R.layout.screen_add_card) {
     }
 
     @SuppressLint("FragmentLiveDataObserve")
-    private fun initObservers() = with(viewModel){
+    private fun initObservers() = with(viewModel) {
         uploadCardLiveData.observe(this@AddCardScreen, uploadCardObserver)
     }
 
     private val uploadCardObserver = Observer<Response<String>> {
-        when(it){
+        when (it) {
             is Response.Loading -> {
                 binding.progress.isVisible = true
             }
             is Response.Failure -> {
                 binding.progress.isVisible = false
-                snackMessage(it.error?:"Not added")
+                snackMessage(it.error ?: "Not added")
             }
             is Response.Success -> {
                 binding.progress.isVisible = false
@@ -67,12 +72,64 @@ class AddCardScreen : Fragment(R.layout.screen_add_card) {
             val cardNumber = etCardNumber.text.toString().trim()
             val validityPeriod = etValidityPeriodOfTheCard.text.toString().trim()
 
-            if (cardNumber.isNotEmpty() && validityPeriod.isNotEmpty()){
-                val request = GetCardsResponse(card_number = cardNumber, validity_period = validityPeriod)
-                viewModel.uploadCards(request)
-            }else{
-                snackMessage("Field not filled")
-            }
+            val request =
+                GetCardsResponse(card_number = cardNumber, validity_period = validityPeriod)
+            viewModel.uploadCards(request)
         }
+
+        etCardNumber.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                check()
+                s?.let {
+                    isCardNumber = s.isNotEmpty()
+                }
+            }
+
+        })
+
+        etValidityPeriodOfTheCard.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                check()
+                s?.let {
+                    isCardDate = s.isNotEmpty()
+                }
+
+                var str = s.toString()
+                if (str.length == 2) {
+                    if (str.toInt() < 32) isValidValiditDate = true
+                    else {
+                        isValidValiditDate = false
+                        etValidityPeriodOfTheCard.error = getString(R.string.invalid_month_day)
+                    }
+                } else if (str.length == 5) {
+                    str = str.substring(str.length - 2)
+                    if (str.toInt() < 13) isValidValiditDate = true
+                    else {
+                        isValidValiditDate = false
+                        etValidityPeriodOfTheCard.error = getString(R.string.invalid_month)
+                    }
+                }
+            }
+        })
+    }
+
+    private fun check() {
+        binding.btnContinue.isEnabled = isCardDate && isCardNumber
     }
 }
